342172016efc5808fe10e1d8cd806b62ebcbc63a
Mohtasham <mohtashammurshid@gmail.com>
2024-10-11 11:12:09 +0800

    add globe to contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,14 +1,8 @@
-import {
-  FaLinkedin,
-  FaInstagram,
-  FaRegEnvelope,
-  FaArrowRight,
-} from "react-icons/fa";
+import { FaLinkedin, FaInstagram, FaRegEnvelope } from "react-icons/fa";
 import { SiFiverr } from "react-icons/si";
-
 import { FaXTwitter } from "react-icons/fa6";
 import DynamicHeading from "@/components/DynamicHeading";
-import { Button } from "@/components/ui/button";
+import Globe from "@/components/ui/globe";
 
 const names = [
   "جهة الاتصال الخاصة بي",
@@ -20,74 +14,56 @@ const names = [
 
 const Page = () => {
   return (
-    <section className="bg-[--background] flex flex-col items-center  ">
+    <section className="bg-[--background] flex flex-col items-center p-4 md:p-8">
+      {/* Globe at the top on small screens */}
+
+      {/* Dynamic Heading */}
       <DynamicHeading names={names} />
-      <div className="flex flex-col md:flex-row w-full h-full">
-        {/* Left: Why Contact Me Card */}
-        <div className="flex-1 p-6 shadow-md rounded-lg items-center justify-center">
-          <h2 className="text-2xl font-semibold mb-8 text-center">
-            Why Contact Me
-          </h2>
-          <p className="text-justify">
-            I am passionate about helping individuals and organizations tackle
-            complex challenges through innovative, thoughtful solutions. Whether
-            you&apos;re seeking collaboration on a project, looking for expert advice
-            in software development, or exploring new opportunities, I am always
-            eager to connect. With a diverse skill set in web development, AI,
-            and machine learning, combined with a dedication to continuous
-            learning, I aim to deliver value in everything I do. Feel free to
-            reach out if you&apos;re ready to collaborate, exchange ideas, or discuss
-            how we can create meaningful impact together.
-          </p>
-          <div className="flex flex-row items-center justify-center gap-6 mt-8">
-            <Button className="bg-transparent  hover:bg-white hover:text-black transition-all">
-              <a
-                href="https://twitter.com/MohtashamCodes"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex flex-row items-center"
-              >
-                My Blog
-                <FaArrowRight className="ml-1" />
-              </a>
-            </Button>
-          </div>
+
+      {/* Layout for content */}
+      <div className="flex flex-col md:flex-row w-full gap-8 mt-8">
+        {/* Left: Globe on larger screens */}
+        <div className="relative hidden md:flex w-full md:w-1/2 items-center justify-center mb-4 md:mb-0 rounded-lg md:pb-60 md:shadow-xl">
+          <Globe />
         </div>
 
         {/* Right: My Socials and Work Contacts */}
-        <div className="flex-1 flex flex-col gap-4 ">
+        <div className="flex-1 flex flex-col ">
           {/* Socials */}
-          <div className="flex-1  p-6 shadow-md rounded-lg items-center justify-center">
-            <h2 className="text-xl  mb-8 font-semibold">My Socials</h2>
+          <div className="p-6 shadow-md rounded-lg flex flex-col items-center md:items-start">
+            <h2 className="text-xl mb-6 font-semibold text-center md:text-left">
+              My Socials
+            </h2>
             <ul className="list-none space-y-4">
-              <li className="flex items-center">
+              {/* Social Links */}
+              <li className="flex items-center justify-center md:justify-start">
                 <a
                   href="https://twitter.com/MohtashamCodes"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="flex flex-row items-center"
+                  className="flex items-center transition-transform duration-300"
                 >
-                  <FaXTwitter className="text-4xl mr-2  hover:scale-110 transition-transform duration-300" />
+                  <FaXTwitter className="text-4xl mr-2 hover:text-[#1DA1F2] hover:scale-110 transition-transform duration-300" />
                   MohtashamCodes
                 </a>
               </li>
-              <li className="flex items-center">
+              <li className="flex items-center justify-center md:justify-start">
                 <a
                   href="https://linkedin.com/in/mohtashammurshid"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="flex flex-row items-center"
+                  className="flex items-center transition-transform duration-300"
                 >
                   <FaLinkedin className="text-4xl mr-2 hover:text-[#0077B5] hover:scale-110 transition-transform duration-300" />
                   mohtashammurshid
                 </a>
               </li>
-              <li className="flex items-center">
+              <li className="flex items-center justify-center md:justify-start">
                 <a
                   href="https://instagram.com/clickswithm"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="flex flex-row items-center"
+                  className="flex items-center transition-transform duration-300"
                 >
                   <FaInstagram className="text-4xl mr-2 hover:text-[#E4405F] hover:scale-110 transition-transform duration-300" />
                   clickswithm
@@ -95,19 +71,31 @@ const Page = () => {
               </li>
             </ul>
           </div>
+
           {/* Work Contacts */}
-          <div className="flex-1  p-6 shadow-md rounded-lg">
-            <h2 className="text-xl font-semibold mb-4">Work Contacts</h2>
+          <div className="p-6 shadow-md rounded-lg flex flex-col items-center md:items-start">
+            <h2 className="text-xl mb-4 font-semibold text-center md:text-left">
+              Work Contacts
+            </h2>
             <ul className="list-none space-y-4">
-              <li className="flex items-center">
-                <FaRegEnvelope className="text-4xl mr-2 hover:text-[#D44638] hover:scale-110 transition-transform duration-300 " />
-                <a href="mailto:mohtashammurshid@gmail.com">
+              {/* Work Links */}
+              <li className="flex items-center justify-center md:justify-start">
+                <a
+                  href="mailto:mohtashammurshid@gmail.com"
+                  className="flex items-center transition-transform duration-300"
+                >
+                  <FaRegEnvelope className="text-4xl mr-2 hover:text-[#D44638] hover:scale-110 transition-transform duration-300" />
                   mohtashammurshid@gmail.com
                 </a>
               </li>
-              <li className="flex items-center">
-                <SiFiverr className="text-5xl mr-2 hover:text-green-500 hover:scale-110 transition-transform duration-300 " />
-                <a href="https://www.fiverr.com/mohtashammadani">
+              <li className="flex items-center justify-center md:justify-start">
+                <a
+                  href="https://www.fiverr.com/mohtashammadani"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center transition-transform duration-300"
+                >
+                  <SiFiverr className="text-5xl mr-2 hover:text-green-500 hover:scale-110 transition-transform duration-300" />
                   mohtashammadani
                 </a>
               </li>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,10 @@ const config: Config = {
   ],
   theme: {
   	extend: {
-		fontFamily: {
-			sans: ['var(--font-geist-sans)'],
-			mono: ['var(--font-geist-mono)'],
-		  },
+  		fontFamily: {
+  			sans: ['var(--font-geist-sans)'],
+  			mono: ['var(--font-geist-mono)']
+  		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
@@ -76,11 +76,20 @@ const config: Config = {
   				to: {
   					height: '0'
   				}
+  			},
+  			orbit: {
+  				'0%': {
+  					transform: 'rotate(0deg) translateY(calc(var(--radius) * 1px)) rotate(0deg)'
+  				},
+  				'100%': {
+  					transform: 'rotate(360deg) translateY(calc(var(--radius) * 1px)) rotate(-360deg)'
+  				}
   			}
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
+  			'accordion-up': 'accordion-up 0.2s ease-out',
+  			orbit: 'orbit calc(var(--duration)*1s) linear infinite'
   		}
   	}
   },
